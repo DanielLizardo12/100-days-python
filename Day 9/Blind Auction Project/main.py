a577e2e51fac69b668848bbd26e1496b21547838
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2025-11-25 16:55:03 -0600

    day 9 completed

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,25 @@
+from art import logo
+
+print(logo)
+
 # TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
+bidders = {}
+
+while True:
+    bidder_name = input("What is your name?: ")
+    bidder_bid = int(input("What's your bid?: "))
+
+    # TODO-2: Save data into dictionary {name: price}
+    bidders[bidder_name] = bidder_bid
+
+    # TODO-3: Whether if new bids need to be added
+    more_bidders_input = input("Are there any other bidders?: (y/n) ").lower()
+    if more_bidders_input == "y":
+        print("\n" * 20)
+    else:
+        break
+
 # TODO-4: Compare bids in dictionary
+key_of_max_bid = max(bidders, key=bidders.get)
 
-
+print(f"The winner is {key_of_max_bid} with a bid of ${bidders[key_of_max_bid]}.")
--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,20 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again.",
+    "item2": "asdasd",
+    "item3": "blabla",
+}
+
+print(programming_dictionary["item2"])
+
+programming_dictionary["loop"] = "something"
+
+empty_dict = {}
+
+print(programming_dictionary)
+
+programming_dictionary["Bug"] = "new data"
+
+print(programming_dictionary)
+
+for key in programming_dictionary:
+    print(key + " : " + programming_dictionary[key])
