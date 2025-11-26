811e6e3df55eb9a22dd5c0794148b35a066d4fcc
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2025-11-26 13:41:57 -0600

    day 10 completed

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,46 @@
+from art import logo
+
 def add(n1, n2):
     return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+def print_operations():
+    for key in calc_dict:
+        print(key)
+
+calc_dict = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+print(logo)
+use_result = False
+
+while True:
+    if use_result:
+        first_number = prev_result
+    else:
+        first_number = float(input("What is the first number?: "))
+
+    print_operations()
+    operation = input("type an operation: ")
+    second_number = float(input("type the second number: "))
+    prev_result = calc_dict[operation](first_number, second_number)
+    print(f"The result of {first_number} {operation} {second_number} "
+          f"is {prev_result}")
+
+    use_result = input("use prev result? (y/n): ") == "y"
+
+
 
 
--- Day 10/Docstrings/task.py
+++ Day 10/Docstrings/task.py
@@ -1,4 +1,5 @@
 def format_name(f_name, l_name):
+    """takes two strings and makes them title"""
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
