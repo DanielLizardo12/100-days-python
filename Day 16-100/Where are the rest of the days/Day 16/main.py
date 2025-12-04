9307560454887045e1679de0f47dab6409bd8dc0
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2025-12-04 11:40:42 -0600

    day 16 inital objects

---
+++ Day 16-100/Where are the rest of the days/Day 16/main.py
@@ -0,0 +1,11 @@
+import another_module
+from turtle import Turtle, Screen
+
+print(another_module.another_variable)
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("DarkSeaGreen")
+timmy.forward(100)
+my_screen = Screen()
+my_screen.exitonclick()
