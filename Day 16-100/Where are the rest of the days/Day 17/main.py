642ab6bc37504dae1294e2f270f6962e1234f6ca
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2025-12-05 13:25:06 -0600

    day 17 added self method

--- Day 16-100/Where are the rest of the days/Day 17/main.py
+++ Day 16-100/Where are the rest of the days/Day 17/main.py
@@ -5,9 +5,22 @@ class User:
         self.id = user_id
         self.username = username
         self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
 user1 = User("001", "daniel")
+user2 = User("002", "daniel2")
 
 print(user1.id)
+print(user1.followers)
+
+user1.follow(user2)
+
+print(user1.following)
+print(user2.followers)
+print(user2.following)
 print(user1.followers)
