891cd34f2a65ed93fa75d8bfef146f0c610f8dd4
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2025-12-05 13:19:48 -0600

    day 12 created constructor

--- Day 16-100/Where are the rest of the days/Day 17/main.py
+++ Day 16-100/Where are the rest of the days/Day 17/main.py
@@ -1,4 +1,13 @@
 class User:
-    pass
 
-user1 = User()
+    def __init__(self, user_id, username):
+        print("i am being created!")
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+
+
+user1 = User("001", "daniel")
+
+print(user1.id)
+print(user1.followers)
