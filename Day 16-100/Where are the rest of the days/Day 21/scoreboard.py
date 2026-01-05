6d0fe845801588fb107238bfc7ae59808b02ad31
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2026-01-05 12:07:16 -0600

    Day 21 add gameover

--- Day 16-100/Where are the rest of the days/Day 21/scoreboard.py
+++ Day 16-100/Where are the rest of the days/Day 21/scoreboard.py
@@ -20,3 +20,7 @@ class Scoreboard(Turtle):
 
     def update_score(self):
         self.write(f"score {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("Game Over", align=ALIGNMENT, font=FONT)
