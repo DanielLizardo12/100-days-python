f1e19b553f5619669734cf3b5e2a1b0104db14df
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2026-01-06 12:45:56 -0600

    Day 22 completed

--- Day 16-100/Where are the rest of the days/Day 22/main.py
+++ Day 16-100/Where are the rest of the days/Day 22/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -11,6 +12,7 @@ screen.tracer(0)
 paddle1 = Paddle((-350, 0))
 paddle2 = Paddle((350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(paddle1.go_up, "w")
@@ -20,7 +22,7 @@ screen.onkey(paddle2.go_down, "l")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -29,5 +31,13 @@ while game_is_on:
 
     if ball.distance(paddle2) < 70 and ball.xcor() > 320 or ball.distance(paddle1) < 70 and ball.xcor() < -320:
         ball.bounce_x()
+
+    if ball.xcor() > 350:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -350:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
