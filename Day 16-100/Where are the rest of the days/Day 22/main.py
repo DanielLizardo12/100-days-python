7a68cb68aecdf2cde89a50e5474f5cf5d97b75df
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2026-01-06 11:19:37 -0600

    Day 22 added paddle

--- Day 16-100/Where are the rest of the days/Day 22/main.py
+++ Day 16-100/Where are the rest of the days/Day 22/main.py
@@ -3,5 +3,29 @@ from turtle import Turtle, Screen
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
+screen.tracer(0)
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(5, 1)
+paddle.penup()
+paddle.goto(350, 0)
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+screen.listen()
+screen.onkey(go_up, "w")
+screen.onkey(go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
