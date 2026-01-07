f31b0ae89fdeb73434ccc45626340c8c2519d148
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2026-01-07 11:53:00 -0600

    added scorebeard

--- Day 16-100/Where are the rest of the days/Day 23/main.py
+++ Day 16-100/Where are the rest of the days/Day 23/main.py
@@ -9,6 +9,7 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 game_is_on = True
 while game_is_on:
--- Day 16-100/Where are the rest of the days/Day 23/scoreboard.py
+++ Day 16-100/Where are the rest of the days/Day 23/scoreboard.py
@@ -1,4 +1,22 @@
+from turtle import Turtle
+
 FONT = ("Arial", 20, "normal")
 
-class Scoreboard():
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.level = 0
+        self.goto(-280, 240)
+        self.update_level()
+
+    def update_level(self):
+        self.clear()
+        self.level += 1
+        self.write(f" level {self.level}", font=("Arial", 30, "bold"))
+
+    def reset_level(self):
+        self.level = 0
+        self.update_level()
