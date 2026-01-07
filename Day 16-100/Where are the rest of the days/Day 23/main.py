855a7326754985888306f88ecaf6d2e1020751a5
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2026-01-07 12:21:13 -0600

    Day 23 completed

--- Day 16-100/Where are the rest of the days/Day 23/main.py
+++ Day 16-100/Where are the rest of the days/Day 23/main.py
@@ -14,11 +14,27 @@ player = Player()
 
 screen.listen()
 screen.onkey(player.move, "w")
+sleep_time = 0.1
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(sleep_time)
+
     if random.randint(1, 6) == 1:
         car_manager.create_car()
     car_manager.move_cars()
+
+    if player.ycor() > 220:
+        scoreboard.update_level()
+        player.reset_position()
+        sleep_time *= 0.8
+        time.sleep(sleep_time)
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            sleep_time = 0.1
+            time.sleep(sleep_time)
+            player.reset_position()
+            scoreboard.reset_level()
+
     screen.update()
