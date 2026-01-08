55eb94319a4012a0c83f3f3aea6f572510574de9
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2026-01-08 14:34:40 -0600

    stored high score in file

--- Day 16-100/Where are the rest of the days/Day 24/scoreboard.py
+++ Day 16-100/Where are the rest of the days/Day 24/scoreboard.py
@@ -7,7 +7,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = int(open("data.txt", "r").read())
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -26,5 +26,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.high_score))
         self.score = 0
         self.update_score()
