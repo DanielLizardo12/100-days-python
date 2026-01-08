7eb3d1184d64a241ae067bb2cc8734bdddf9ebe9
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2026-01-08 14:17:42 -0600

    Day 24 open and read files

--- Day 16-100/Where are the rest of the days/Day 24/snake.py
+++ Day 16-100/Where are the rest of the days/Day 24/snake.py
@@ -7,17 +7,14 @@ LEFT = 180
 RIGHT = 0
 
 class Snake:
-
     def __init__(self):
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
 
-
     def create_snake(self):
         for position in STARTING_POSITIONS:
             self.add_segment(position)
-
 
     def add_segment(self, position):
         new_segment = Turtle("square")
@@ -25,7 +22,7 @@ class Snake:
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
-        
+
     def reset(self):
         for segment in self.segments:
             segment.goto(1000,1000)
@@ -35,7 +32,6 @@ class Snake:
 
     def extend(self):
         self.add_segment(self.segments[-1].position())
-
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
