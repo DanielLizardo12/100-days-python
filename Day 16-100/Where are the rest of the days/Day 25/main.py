277ae6111404c26845bed048c643db9e7a260c9d
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2026-01-09 13:40:55 -0600

    created csv with pandas

--- Day 16-100/Where are the rest of the days/Day 25/main.py
+++ Day 16-100/Where are the rest of the days/Day 25/main.py
@@ -1,14 +1,18 @@
-import csv
 import pandas
 
-with open('weather_data.csv') as f:
-    reader = csv.reader(f)
-    next(reader)
-    temperatures = []
-    for row in reader:
-        temperatures.append(int(row[1]))
-    #print(temperatures)
-
-
 data = pandas.read_csv('weather_data.csv')
-print(data["temp"])
+print(type(data))
+print(data["temp"].mean())
+print(data["temp"].max())
+print(data.temp.max())
+
+print(data[data.temp == data.temp.max()].condition)
+print((data[data.day == "Monday"].temp * 9 / 5) + 32)
+
+data_dict = {
+    "students": ["amy", "dan", "david"],
+    "scores": [70, 100, 90],
+}
+
+data = pandas.DataFrame(data_dict)
+data.to_csv("students.csv")
