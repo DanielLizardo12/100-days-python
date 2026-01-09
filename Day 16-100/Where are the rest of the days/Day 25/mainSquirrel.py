f8a021470666f3eba999cf7371d162d3db597dba
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2026-01-09 17:08:13 -0600

    Day 25 squirrel color count

---
+++ Day 16-100/Where are the rest of the days/Day 25/mainSquirrel.py
@@ -0,0 +1,15 @@
+import pandas
+
+data = pandas.read_csv("squirrel_count.csv")
+gray_squirrel = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrel = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrel = len(data[data["Primary Fur Color"] == "Black"])
+
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrel, red_squirrel, black_squirrel],
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_color_count.csv")
