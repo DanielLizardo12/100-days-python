7fb776eb2e7fc4d77defe2df70de1b0f90b7cfb0
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2026-01-12 11:31:33 -0600

    Day 26 dict comprehension

--- Day 16-100/Where are the rest of the days/Day 26/main.py
+++ Day 16-100/Where are the rest of the days/Day 26/main.py
@@ -1,3 +1,5 @@
+import random
+
 numbers = [1,2,3]
 
 new_number = [n + 1 for n in numbers]
@@ -10,3 +12,9 @@ print(letter_list)
 
 range_list = [n * 2 for n in range(1,5) if n != 2]
 print(range_list)
+
+names = ["dan", "dan1", "dan2", "dan3", "dan4", "dan5"]
+
+student_scores = {student:random.randint(1, 100) for student in names}
+passed_students = {student:score for student, score in student_scores.items() if score >= 70}
+print(passed_students)
