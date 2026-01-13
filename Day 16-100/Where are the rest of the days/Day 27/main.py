577e358c5f34472448759de556293fc06cffcdef
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2026-01-13 14:54:44 -0600

    Day 27 grid

--- Day 16-100/Where are the rest of the days/Day 27/main.py
+++ Day 16-100/Where are the rest of the days/Day 27/main.py
@@ -6,17 +6,21 @@ def button_clicked():
 window = tkinter.Tk()
 window.title("First GUI program")
 window.minsize(500, 300)
+window.config(padx=20, pady=20)
 
 #Label
 my_label = tkinter.Label(text="hello", font=("Arial", 25, "bold"))
-my_label.pack()
+my_label.grid(column=0, row=0)
 
 ##button
 button = tkinter.Button(text="Click here", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
 
 #entry
 input_tk = tkinter.Entry(width=10)
-input_tk.pack()
+input_tk.grid(column=3, row=2)
+
+button_1 = tkinter.Button(text="Click here again", command=button_clicked)
+button_1.grid(column=2, row=0)
 
 window.mainloop()
