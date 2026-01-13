0b981091e36b2b1ae6a39b95a313978a5367b16d
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2026-01-13 13:55:04 -0600

    Day 27 buttons and inputs

--- Day 16-100/Where are the rest of the days/Day 27/main.py
+++ Day 16-100/Where are the rest of the days/Day 27/main.py
@@ -1,11 +1,22 @@
 import tkinter
 
+def button_clicked():
+    my_label["text"] = input_tk.get()
+
 window = tkinter.Tk()
 window.title("First GUI program")
 window.minsize(500, 300)
 
 #Label
 my_label = tkinter.Label(text="hello", font=("Arial", 25, "bold"))
-my_label.pack(side="left")
+my_label.pack()
+
+##button
+button = tkinter.Button(text="Click here", command=button_clicked)
+button.pack()
+
+#entry
+input_tk = tkinter.Entry(width=10)
+input_tk.pack()
 
 window.mainloop()
