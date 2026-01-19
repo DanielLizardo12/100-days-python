2bf14f4f35d49b8225bfe361960f9e39a943436d
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2026-01-19 11:51:53 -0600

    Day 29, store passwords

--- Day 16-100/Where are the rest of the days/Day 29/main.py
+++ Day 16-100/Where are the rest of the days/Day 29/main.py
@@ -1,6 +1,32 @@
+import json
+import os
 import tkinter
 
 FONT_NAME = "Courier"
+FILE_NAME = "passwords.txt"
+
+data = []
+if os.path.exists(FILE_NAME):
+    with open(FILE_NAME, "r") as file:
+        data = json.load(file)
+else:
+    data = []
+
+
+def store_data():
+    data.append({
+        "website": website_entry.get(),
+        "email": email_entry.get(),
+        "password": password_entry.get(),
+    })
+    with open(FILE_NAME, "w") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+
+    website_entry.delete(0, tkinter.END)
+    email_entry.delete(0, tkinter.END)
+    email_entry.insert(0, "email@gmail.com")
+    password_entry.delete(0, tkinter.END)
+
 
 window = tkinter.Tk()
 window.title("Password Manager")
@@ -25,9 +51,11 @@ password_label.grid(column=0, row=4, sticky=tkinter.E, pady=10)
 
 website_entry = tkinter.Entry(width=50, font=("Arial", 20, "bold"))
 website_entry.grid(column=1, row=2, columnspan=2, sticky=tkinter.W, pady=10)
+website_entry.focus()
 
 email_entry = tkinter.Entry(width=50, font=("Arial", 20, "bold"))
 email_entry.grid(column=1, row=3, columnspan=2, sticky=tkinter.W, pady=10)
+email_entry.insert(0, "email@gmail.com")
 
 password_entry = tkinter.Entry(width=33, font=("Arial", 20, "bold"))
 password_entry.grid(column=1, row=4, sticky=tkinter.W, pady=10)
@@ -35,7 +63,7 @@ password_entry.grid(column=1, row=4, sticky=tkinter.W, pady=10)
 gen_pass_button = tkinter.Button(text="Generate Password", width=20, font=("Arial", 14, "bold"))
 gen_pass_button.grid(column=2, row=4, sticky=tkinter.W, pady=10)
 
-add_pass_button = tkinter.Button(text="add", width=62, font=("Arial", 14, "bold"))
+add_pass_button = tkinter.Button(text="add", width=62, font=("Arial", 14, "bold"), command=store_data)
 add_pass_button.grid(column=1, row=5, columnspan=2, sticky=tkinter.W, pady=10)
 
 
