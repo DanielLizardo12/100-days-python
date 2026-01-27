53b68270db06d7da1ed55c84f05ed342c5e91ee5
Wesley Daniel Lizardo González <wdaniel.lizardo1@gmail.com>
2026-01-27 12:30:01 -0600

    Day 31 initial interface

---
+++ Day 16-100/Where are the rest of the days/Day 31/main.py
@@ -0,0 +1,27 @@
+import tkinter
+
+FONT_NAME = "Courier"
+BG_COLOR = "#B5FFD9"
+YES_COLOR = "#73BA5D"
+NO_COLOR = "#BA5D5D"
+
+window = tkinter.Tk()
+window.title("Flashy")
+window.config(padx=20, pady=20, bg=BG_COLOR)
+
+canvas = tkinter.Canvas(width=400, height=250, bg="white")
+canvas.grid(row=0, column=0, columnspan=2, rowspan=2)
+
+language_label = tkinter.Label(text="Language", bg="white", font=(FONT_NAME, 25))
+language_label.grid(row=0, column=0, columnspan=2)
+
+word_label = tkinter.Label(text="Word", bg="white", font=(FONT_NAME, 35, "bold"))
+word_label.grid(row=1, column=0, columnspan=2)
+
+no_entry = tkinter.Button(width=10, height=3, text="X", font=(FONT_NAME, 10, "bold"), bg=NO_COLOR)
+no_entry.grid(row=2, column=0, pady=20)
+
+yes_entry = tkinter.Button(width=10, height=3, text="✓", font=(FONT_NAME, 10, "bold"), bg=YES_COLOR)
+yes_entry.grid(row=2, column=1, pady=20)
+
+window.mainloop()
